64c320df862e544f9ab69454e5e10a789bf084cc
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-12-21 15:39:16 +0100

    rest template bean

--- src/main/java/com/taskflow/service2/configuration/SecurityConfiguration.java
+++ src/main/java/com/taskflow/service2/configuration/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -62,5 +63,10 @@ public class SecurityConfiguration {
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
