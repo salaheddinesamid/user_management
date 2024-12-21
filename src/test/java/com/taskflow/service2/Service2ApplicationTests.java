99e715276dc552ccaf1eed80809e35153b3f664f
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-12-21 15:38:30 +0100

    authentication test

--- src/test/java/com/taskflow/service2/Service2ApplicationTests.java
+++ src/test/java/com/taskflow/service2/Service2ApplicationTests.java
@@ -1,19 +1,41 @@
 package com.taskflow.service2;
 
+import com.taskflow.service2.dto.LoginDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.client.RestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Service2ApplicationTests {
 
+	@Autowired
+	RestTemplate restTemplate;
+
     @Test
 	void contextLoads() {
 
+	}
+
+	@Test
+	void authenticationTest(){
+		String url = "http://localhost:8080/api/user/authenticate";
+		LoginDTO loginDTO = new LoginDTO();
+		loginDTO.setEmail("faterAhmed@gmail.com");
+		loginDTO.setPassword("a");
+		HttpEntity<LoginDTO> httpEntity = new HttpEntity<>(loginDTO);
+		assertThat(this.restTemplate.exchange(
+				url,
+				HttpMethod.POST,
+				httpEntity,
+				String.class
+		)).isEqualTo("Invalid credentials");
 	}
 }
